1f1d486626b8246ca280f31cf64de34df3a7e9e4
Mark McEver <mark@fastmail.net>
2017-12-11 09:45:29 -0600

    Fixed several issues with my initial pseudo-code for this

--- SharedLibraryExample.php
+++ SharedLibraryExample.php
@@ -1,19 +1,18 @@
 <?php
-namespace Vanderbilt\SharedLibraryExample
+namespace Vanderbilt\SharedLibraryExample;
 
 class SharedLibraryExample{
 	private $number;
 
-	public static echoCachedRandomNumber(){
+	public static function echoCachedRandomNumber(){
 		if(!isset(self::$number)){
 			echo "This is the first call to " . __FUNCTION__ . "().  A new random number will be generated and cached.";
 			self::$number = rand();
 		}
 		else{
-			echo "This is a subsequent call to " . __FUNCTION__ . "().  The previously generated number will be returned."
+			echo "This is a subsequent call to " . __FUNCTION__ . "().  The previously generated number will be returned.";
 		}
 
-		echo "<br>";
-		echo "The number is: ": self::$number;
+		echo "<br>The number is: " . self::$number . "<br><br>";
 	}	
 }
